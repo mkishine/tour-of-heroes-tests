c77dfbcb6303a9a7b20f8ad1e8e1cb33ecd9f792
mkishine <mkishine@yahoo.com>
2016-10-08 19:59:37 -0400

    https://angular.io/docs/ts/latest/guide/user-input.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
-
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero }}
+      </li>
+    </ul>
+    <click-me></click-me>
+    <br>
+    <key-up1></key-up1>
+    <br>
+    <key-up2></key-up2>
+    <br>
+    <key-up3></key-up3>
+    <br>
+    <key-up4></key-up4>
+    <br>
+    <little-tour></little-tour>
+`
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ClickMeComponent }  from './click-me.component';
+import { KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_v3, KeyUpComponent_v4 }  from './keyup.components';
+import { LittleTourComponent }  from './little-tour.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ClickMeComponent,
+    KeyUpComponent_v1,
+    KeyUpComponent_v2,
+    KeyUpComponent_v3,
+    KeyUpComponent_v4,
+    LittleTourComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
